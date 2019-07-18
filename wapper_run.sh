6821f4e5c7c2b54147b05275d102d519b1ac5d46
Mukul Malhotra <smilemukul2005@gmail.com>
2019-07-18 01:31:46 -0900

    modified mstakx files

--- wapper_run.sh
+++ wapper_run.sh
@@ -1,7 +1,7 @@
-!#/bin/bash
+#!/bin/bash
 
 ## Prerequistie
 
 yum --enablerepo=epel install ansible python git -y
 
-ansible-playbook provision_kube_cluster.yml
+ansible-playbook provision_kube_cluster.yaml
