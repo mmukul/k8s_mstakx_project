56d0d27bca2c47c52b82c5f2da82a37a00cd884b
Mukul Malhotra <smilemukul2005@gmail.com>
2019-07-18 12:00:12 -0900

    modified mstakx files

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 # Provision Kubernetes Cluster with Ansible
 
+## Execute:step1
 $ ansible-playbook provision_kube_cluster.yml
 
+## Execute:step2
+$ ansible-playbook playbooks/join_kube_nodes.yaml
--- wapper_run.sh
+++ wapper_run.sh
@@ -2,6 +2,10 @@
 
 ## Prerequistie
 
+yum install epel-release -y
 yum --enablerepo=epel install ansible python git -y
 
+git clone https://github.com/mmukul/k8s_mstakx_project.git
+
+# Ansible playbook to provision the kube master infrastructure
 ansible-playbook provision_kube_cluster.yaml
