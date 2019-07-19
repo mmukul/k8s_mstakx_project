50970547fbca5e811df4c1d1ab1b5561b0351af8
mmalh47 <smilemukul2005@gmail.com>
2019-07-19 22:20:55 +0530

    modified mstakx files

--- README.md
+++ README.md
@@ -1,7 +1,20 @@
 # Provision Kubernetes Cluster with Ansible
 
-## Execute:step1
+## AWS Instance Provision Requirment
+
+AMI: ami-0d2692b6acea72ee6
+Instance Type: t2.medium
+   - vcpu: 2
+   - Memory: 4 GiB
+Instance count: 2
+Instance tag:
+   - tag-1: kube-master
+   - tag-2: kube-worker
+
+## Step1: Execute script from kube-master
+
 $ ansible-playbook provision_kube_cluster.yml
 
-## Execute:step2
-$ ansible-playbook playbooks/join_kube_nodes.yaml
+## Step1: Execute script from kube-worker
+
+
--- wapper_run.sh
+++ wapper_run.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
+!#/bin/bash
 
 ## Prerequistie
 
-yum install epel-release -y
-yum --enablerepo=epel install ansible python git -y
+yum install epel-release python  -y
+yum --enablerepo=epel install ansible -y
+ansible-playbook kubectl.yaml --extra-vars "kubernetes_api_endpoint=kube-375210502.eu-west-1.elb.amazonaws.com"
+ssh-agent bash
+ssh-add ~/.ssh/keypair.pem
 
-git clone https://github.com/mmukul/k8s_mstakx_project.git
 
-# Ansible playbook to provision the kube master infrastructure
-ansible-playbook provision_kube_cluster.yaml
+kubectl create -f playbook/redis-master-deployment.yaml
+kubectl create -f playbook/redis-master-service.yaml
+kubectl create -f playbook/redis-slave-deployment.yaml
+kubectl create -f playbook/frontend-deployment.yaml
+kubectl create -f playbook/frontend-service.yaml
