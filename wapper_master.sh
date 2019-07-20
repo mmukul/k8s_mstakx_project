cae9f0ecb4324fff539d0f275dde41a64fa1fa22
mmalh47 <smilemukul2005@gmail.com>
2019-07-20 12:48:11 +0530

    modified mstakx files

--- wapper_master.sh
+++ wapper_master.sh
@@ -5,9 +5,5 @@ yum install epel-release python git  -y
 yum --enablerepo=epel install ansible -y
 
 
-## Clone git repo
-git clone https://github.com/mmukul/k8s_mstakx_project.git
-
-
 ## Execute ansible playbook on kubernetes master node
 ansible-playbook provision_kube_cluster.yaml
--- wapper_worker.sh
+++ wapper_worker.sh
@@ -4,10 +4,6 @@
 yum install epel-release python git  -y
 yum --enablerepo=epel install ansible -y
 
-
-## Clone git repo
-git clone https://github.com/mmukul/k8s_mstakx_project.git
-
  
 ## Execute ansible playbook on kubernetes worker node
 ansible-playbook playbooks/configure_worker_nodes.yaml
